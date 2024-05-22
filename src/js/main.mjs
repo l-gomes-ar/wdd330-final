cac27ccd95f61c3aea12b4df90ab2dae33e04008
l-gomes-ar <lgaraujo021@gmail.com>
2024-05-22 21:01:37 +0100

    Added SearchBar.mjs + added searching functionality in homepage + deleted comments

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -1,7 +1,10 @@
 import AssetsDetails from "./AssetsDetails.mjs";
+import SearchBar from "./SearchBar.mjs";
 import { loadStaticTemplates } from "./utils.mjs";
 
 loadStaticTemplates();
+const search = new SearchBar();
+search.init();
 
 const parentElem = document.querySelector("#home-currencies-container");
 const assets = new AssetsDetails(parentElem);
